c548a6e981b361042b2a509115cd64a7d25aa590
JuliethAnarosa <america02anarosa@gmail.com>
2023-09-03 22:34:27 -0500

    task

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+collect 10 random numbers using an async comprehension
+and return random numbers
+'''
+import asyncio
+from typing import List
+from random import uniform
+from importlib import import_module
+
+# Import async_generator using import_module
+async_generator = import_module("0-async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''
+    collects 10 random number using async comp
+    '''
+    return [x async for x in async_generator()]
---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function to measure runtime
+"""
+import asyncio
+import time
+from typing import List
+from random import uniform
+from importlib import import_module
+
+async_comprehension_module = import_module("1-async_comprehension")
+async_comprehension = async_comprehension_module.async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measures the total runtime for running
+    async_comprehension four times in parallel.
+    """
+    start_time = time.time()
+
+    tasks = [async_comprehension() for _ in range(4)]
+    await asyncio.gather(*tasks)
+
+    end_time = time.time()
+    total_runtime = end_time - start_time
+    return total_runtime
